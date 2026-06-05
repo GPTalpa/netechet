483cf2b4dea2cd56cbb5fc9aaea637c12c6cb3a9
GPTalpa <120nik01@mail.ru>
2026-06-05 21:39:13 +0200

    fix

--- components/YandexMetrika/YandexMetrika.tsx
+++ components/YandexMetrika/YandexMetrika.tsx
@@ -10,28 +10,14 @@ export default function YandexMetrika() {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),
-              a=e.getElementsByTagName(t)[0],
-              k.async=1,
-              k.src=r,
-              a.parentNode.insertBefore(k,a)
-            })(window, document,'script',
-            'https://mc.yandex.ru/metrika/tag.js?id=106983124',
-            'ym');
-
-            ym(106983124, 'init', {
-              webvisor:true,
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              ecommerce:"dataLayer"
-            });
+             (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106983124', 'ym');
+ 
+    ym(106983124, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
           `,
         }}
       />
